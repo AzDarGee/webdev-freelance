4976cc9ee75fc21858c8399de489a58bf55d1bc0
AzDarGee <ashishdarji88@gmail.com>
2017-01-08 23:16:48 -0500

    added minify and uglify functions in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,9 @@ gulp.task('sass', function() {
   return gulp.src('components/stylesheets/**/*.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest('css'))
-      .pipe(rename({suffix: '.min'}))
+      .pipe(rename({
+        suffix: '.min'
+      }))
       .pipe(minifycss())
       .pipe(gulp.dest('dist/css'))
       .pipe(gzip(gzip_options))
@@ -28,16 +30,49 @@ gulp.task('sass', function() {
       .pipe(browserSync.stream());
 });
 
+// Uglify JS
 gulp.task('compress', function() {
   return gulp.src('components/scripts/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(gzip(gzip_options))
     .pipe(gulp.dest('dist/js'))
     //Browser sync will watch changes and reload on save
     .pipe(browserSync.stream());
 });
+
+// Uglify Plugins
+gulp.task('uglifyPlugins', function() {
+  return gulp.src(['components/libs/bootstrap/dist/js/bootstrap.js',
+    'components/libs/jquery/dist/jquery.js'])
+    .pipe(rename({
+      suffix: '.min',
+      extname: '.js'
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    //Browser sync will watch changes and reload on save
+    .pipe(browserSync.stream());
+});
+
+// Minify Plugins
+gulp.task('minifyPlugins', function() {
+  return gulp.src(['components/libs/bootstrap/dist/css/bootstrap.css'])
+    .pipe(rename({
+      suffix: '.min',
+      extname: '.css'
+    }))
+    .pipe(minifycss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'))
+    //Browser sync will watch changes and reload on save
+    .pipe(browserSync.stream());
+});
+
+// Build Task - Run Uglify & Minify Plugins
+gulp.task('build', ['uglifyPlugins', 'minifyPlugins']);
 
 /* Watch Files For Changes */
 gulp.task('watch', function() {
@@ -57,4 +92,4 @@ gulp.task('watch', function() {
 
 });
 
-gulp.task('default', ['sass', 'compress', 'watch']);
+gulp.task('default', ['sass', 'compress', 'uglifyPlugins', 'minifyPlugins', 'watch']);
