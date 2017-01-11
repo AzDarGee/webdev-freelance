e47e9550608c8d1ee841349e40d7fa36cd20cc3d
AzDarGee <ashishdarji88@gmail.com>
2017-01-10 21:47:11 -0500

    image resizer and optimizer added to template

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+// Import Gulp Plugins
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
@@ -7,6 +9,8 @@ var gzip = require('gulp-gzip');
 var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var imageresize = require('gulp-image-resize');
 var cache = require('gulp-cache');
 var gulpUtil = require('gulp-util');
 
@@ -76,11 +80,11 @@ gulp.task('minifyPlugins', function() {
 });
 
 // Image Task
-gulp.task('images', function(){
-  gulp.src('components/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/images/'));
-});
+// gulp.task('images', function(){
+//   gulp.src('components/images/**/*')
+//     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+//     .pipe(gulp.dest('dist/images/'));
+// });
 
 // Build Task - Run Uglify & Minify Plugins
 gulp.task('build', ['uglifyPlugins', 'minifyPlugins']);
@@ -104,3 +108,76 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['sass', 'compress', 'uglifyPlugins', 'minifyPlugins', 'images', 'watch']);
+
+/**
+ * Make sure Graphicsmagick is installed on your system
+ * osx: brew install graphicsmagick
+ * ubuntu: apt-get install graphicsmagick
+ *
+ * Install these gulp plugins
+ * glup, gulp-image-resize, gulp-imagemin and imagemin-pngquant
+ *
+ * Group images according to their output dimensions.
+ * (ex: place all portfolio images into "images/portfolio"
+ * and all background images into "images/bg")
+ *
+ **/
+
+// set the folder name and the relative paths
+// in the example the images are in ./assets/images
+// and the public directory is ../public
+var paths = {
+    folder: 'images/',
+    src: 'components/',
+    dest: 'dist/'
+}
+
+// create an array of image groups (see comments above)
+// specifying the folder name, the ouput dimensions and
+// whether or not to crop the images
+var images = [
+    { folder: 'bg', width: 1200, crop: false },
+    { folder: 'categories', width: 500, height: 330, crop: true },
+    { folder: 'projects', width: 800, height: 500, crop: true },
+    { folder: 'profile', width: 150, height: 150, crop: false }
+];
+
+// images gulp task
+gulp.task('images', function () {
+
+    // loop through image groups
+    images.forEach(function(type){
+
+    	// build the resize object
+        var resize_settings = {
+            width: type.width,
+            crop: type.crop,
+            // never increase image dimensions
+            upscale : false
+        }
+        // only specify the height if it exists
+        if (type.hasOwnProperty("height")) {
+            resize_settings.height = type.height
+        }
+
+        gulp
+
+        // grab all images from the folder
+        .src(paths.src+paths.folder+type.folder+'/**/*')
+
+        // resize them according to the width/height settings
+        .pipe(imageresize(resize_settings))
+
+        // optimize the images
+        .pipe(imagemin({
+            progressive: true,
+            // set this if you are using svg images
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+
+        // output each image to the dest path
+        // maintaining the folder structure
+        .pipe(gulp.dest(paths.dest+paths.folder+type.folder));
+    });
+});
