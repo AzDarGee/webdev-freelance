4f2bc9f1f535f9ecfe5d91ed566877c9df4db8b2
AzDarGee <ashishdarji88@gmail.com>
2017-01-10 23:34:21 -0500

    added some blurring and transition delays - all through css. no jquery needed

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -1,2 +1,11 @@
 // Initialize BootStrap Material Design
 // $.material.init()
+
+// $(document).ready(function() {
+//   $('.profile-pic').mouseenter(function() {
+//     $(this).addClass("no-blur");
+//   });
+//   $('.profile-pic').mouseleave(function() {
+//     $(this).removeClass("no-blur");
+//   });
+// });
