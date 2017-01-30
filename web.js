bf31fe0fb6a580f5441d49d013422f5c4e730c9c
AzDarGee <ashishdarji88@gmail.com>
2017-01-30 17:15:05 -0500

    update

--- web.js
+++ web.js
@@ -3,12 +3,12 @@ var app = express();
 var path = require('path');
 
 
-
 // Define the port to run on
 app.set('port', 8080);
 
 // we are specifying the html directory as another public directory
 app.use(express.static(path.join(__dirname, '.')));
+
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
